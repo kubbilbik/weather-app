c2c878971f987a3b67bb98907940d8fe6799dd25
kubbilbik <76406996+kubbilbik@users.noreply.github.com>
2024-01-14 11:53:36 -0500

    try catch added

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ function App() {
   const [weather, setWeather] = useState();
   const getWearherData = async (lat , lon ) =>{
     const key = process.env.API_KEY;
-    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`);
-    setWeather(data);
-  }  
+
+    try{
+      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`);
+      setWeather(data);
+    }catch{
+      alert("error fetching data");
+    }
+  };
  
     return (
     <div className="App">
